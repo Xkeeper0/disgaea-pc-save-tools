8a80fd8c3c6f2ce889ae77c9935e0c685b620cbb
Xkeeper <xkeeper@gmail.com>
2016-03-13 22:06:28 -0700

    Correct character count

--- disgaea/savedata.php
+++ disgaea/savedata.php
@@ -6,7 +6,7 @@
 
 		protected	$_data			= "";
 		protected	$_dataChunks	= array(
-			'characters'		=> array( 'start' => 0x0BB8, 'length' => 0x06B8,	'type' => '\Disgaea\Data\Character',	'count' => 0x7F),
+			'characters'		=> array( 'start' => 0x0BB8, 'length' => 0x06B8,	'type' => '\Disgaea\Data\Character',	'count' => 0x80),
 			);
 
 
