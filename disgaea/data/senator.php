9f3598638e4e8827eefd86d6d409304be01fff30
Xkeeper <xkeeper@gmail.com>
2016-03-20 12:39:13 -0700

    Add Senator struct + update savedata struct
    aye

---
+++ disgaea/data/senator.php
@@ -0,0 +1,90 @@
+<?php
+
+	namespace Disgaea\Data;
+
+
+	class Senator extends \Disgaea\DataStruct {
+
+		public static $size			= 0x020;
+		protected	$_data			= "";
+		protected	$_dataChunks	= array(
+
+			'level'				=> array( 'start' => 0x0000, 'length' => 0x0002, 'type' => 'i'),
+			'class'				=> array( 'start' => 0x0002, 'length' => 0x0002, 'type' => 'i'),
+
+			'attendance'		=> array( 'start' => 0x0004, 'length' => 0x0002, 'type' => 'i'),
+			'killed'			=> array( 'start' => 0x0006, 'length' => 0x0002, 'type' => 'i'),
+
+			// Possibly "uniquer"
+			'namebank'			=> array( 'start' => 0x0008, 'length' => 0x0001, 'type' => 'i'),
+			'nameindex'			=> array( 'start' => 0x0009, 'length' => 0x0001, 'type' => 'i'),
+			
+			// Unknown value, always seems to be 0?
+			'unknown'			=> array( 'start' => 0x000A, 'length' => 0x0001, 'type' => 'i'),
+
+			// This seems to be another magic number like "favor"
+			// Values are around 0x100 +/- some amount
+			// Setting it all to the same gives the same desire across all senators for a given rarity
+			// How it works is not known exactly but who cares
+			'raritypreference'	=> array( 'start' => 0x000B, 'length' => 0x0002, 'type' => 'i'),
+
+			'name'				=> array( 'start' => 0x000D, 'length' => 0x0010, 'type' => 's'),
+
+			// This is actually + 0x7F & 0x80; so a max "love" will be 17F, max loathe 80
+			// Probably so game can add +/- value to vote and test bit 8 to see if they vote yes
+			'favor'				=> array( 'start' => 0x001E, 'length' => 0x0002, 'type' => "i"),
+
+			);
+
+
+		public function getFavorText() {
+			$f		= $this->getChunk('favor');
+			$f		= ($f & 0x7F) - 0x80;
+
+			$out	= "???";
+			if ($f < -40) {
+				$out    = "Loathe";
+			} elseif ($f < -26) {
+				$out    = "Total opposition";
+			} elseif ($f < -16) {
+				$out    = "Strongly against";
+			} elseif ($f < -11) {
+				$out    = "Against";
+			} elseif ($f < -5) {
+				$out    = "Leaning no";
+			} elseif ($f < 4) {
+				$out    = "Either way";
+			} elseif ($f < 11) {
+				$out    = "Leaning yes";
+			} elseif ($f < 15) {
+				$out    = "In favor of";
+			} elseif ($f < 25) {
+				$out    = "Strongly for";
+			} elseif ($f < 39) {
+				$out    = "Total support";
+			} elseif ($f < 127) {
+				$out    = "Love";
+			}
+
+			return sprintf("(%4d) %s", $f, $out);
+		}
+
+
+
+		public function __toString() {
+			try {
+			return sprintf("Lv %4d, %-16s - attend %5d - killed %5d - unk %02x - %-25s - %s", 
+				$this->getChunk("level"),
+				\Disgaea\Data\Id::getClass($this->getChunk("class")),
+				$this->getChunk("attendance"),
+				$this->getChunk("killed"),
+				$this->getChunk("unknown"),
+				$this->getFavorText(),
+				$this->getChunk("name")
+				);
+			} catch (\Exception $e) {
+				return $e->getMessage();
+			}
+		}
+
+	}
--- disgaea/savedata.php
+++ disgaea/savedata.php
@@ -6,12 +6,40 @@
 
 		protected	$_data			= "";
 		protected	$_dataChunks	= array(
+
+			// Character data
+			// Pre-filled with your main team at the start, a handful of Pleinairs,
+			// a large group of "Ark" characters with Laharl's picture and 1 in all stats,
+			// followed by more Pleinairs, the castle NPCs, and finally the story characters
+			// like Gordon and Flonne (the very last character in the list)
 			'characters'		=> array( 'start' => 0x000BB8, 'length' => 0x06B8,	'type' => '\Disgaea\Data\Character',	'count' => 0x80),
+			// Character data ends around 0x367B8 (+/- 6b8, math is hard at 4:30 AM)
+
+
+			// There are supposedly 0x200 of these but everything after 0x190 is
+			// a blank "Ark" senator (with 0 in everything of course)
+			'senators'			=> array( 'start' => 0x0367B8, 'length' => 0x0020,	'type' => '\Disgaea\Data\Senator',	'count' => 0x200),
+
 
 			// # of characters in current party
 			// Seems to be 2bytes but max is 0x7F without Problems happening
 			// Castle NPCs and such are near the end
 			'charactercount'	=> array( 'start' => 0x044830, 'length' => 0x0002,	'type' => 'i'),
+
+
+			'bgvolume'	=> array( 'start' => 0x044878, 'length' => 0x0001, 'type' => 'i'),
+			'vovolume'	=> array( 'start' => 0x044879, 'length' => 0x0001, 'type' => 'i'),
+			'sevolume'	=> array( 'start' => 0x04487a, 'length' => 0x0001, 'type' => 'i'),
+
+
+			// Save Shop, Music Shop, Ark (? filler)
+			// These characters are regenerated if they are not the correct class?
+			'extranpcs'			=> array( 'start' => 0x045188, 'length' => 0x06B8,	'type' => '\Disgaea\Data\Character',	'count' => 0x03),
+
+
+
+			// Setting to all FF didn't do anything, save files all have 00s so far
+			'unused'	=> array( 'start' => 0x0465b0, 'length' => 0x22C7, 'type' => "h"),
 			);
 
 
