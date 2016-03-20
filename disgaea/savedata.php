27039a194d04af380b9928cbc14da87952b227e7
Xkeeper <xkeeper@gmail.com>
2016-03-19 18:47:36 -0700

    Add new fields to savedata struct

--- disgaea/savedata.php
+++ disgaea/savedata.php
@@ -6,7 +6,12 @@
 
 		protected	$_data			= "";
 		protected	$_dataChunks	= array(
-			'characters'		=> array( 'start' => 0x0BB8, 'length' => 0x06B8,	'type' => '\Disgaea\Data\Character',	'count' => 0x80),
+			'characters'		=> array( 'start' => 0x000BB8, 'length' => 0x06B8,	'type' => '\Disgaea\Data\Character',	'count' => 0x80),
+
+			// # of characters in current party
+			// Seems to be 2bytes but max is 0x7F without Problems happening
+			// Castle NPCs and such are near the end
+			'charactercount'	=> array( 'start' => 0x044830, 'length' => 0x0002,	'type' => 'i'),
 			);
 
 
